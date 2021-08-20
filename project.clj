c1d43ba617e17d9d29ef9f3a4458182896b5863e
kero_hirake <k16.h.white@gmail.com>
2021-08-21 00:44:35 +0900

    開発用のミドルウェアを追加し、それに伴って prone と environ を追加

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject todo-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.9.3"]
+                 [compojure "1.6.2"]
+                 [environ "1.2.0"]]
+  :plugins [[lein-environ "1.2.0"]]
+  :repl-options {:init-ns todo-clj.core}
+  :profiles 
+  {:dev {:dependencies [[prone "0.8.2"]]
+         :env {:dev true}}})
